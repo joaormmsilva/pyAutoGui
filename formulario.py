3324cf0d8c44afcba08182b8ddcdd66f37b4c23f
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-09-17 07:56:07 -0300

    Update formulario.py

--- formulario.py
+++ formulario.py
@@ -5,62 +5,64 @@ idade = [20,56,12]
 nota = [3,5,1]
 comentario = ['bom','top','ruim']
 
-time.sleep(2)
+pyautogui.mouseInfo()
+pyautogui.alert('Abra o fomulario')
 for i in range(3):
     pyautogui.click()
     
 
-    pyautogui.moveTo(415,435, duration=0.25)
+    pyautogui.moveTo(677,430, duration=0.25)
     pyautogui.click()
     pyautogui.write(nome[i])
 
     pyautogui.scroll(-200)
     
-    pyautogui.moveTo(344,422, duration=0.25)
+    pyautogui.moveTo(694,407, duration=0.25)
     pyautogui.click()
     pyautogui.write(filme[i])
 
-    pyautogui.moveTo(316,566, duration=0.25)
+    pyautogui.moveTo(724,577, duration=0.25)
     pyautogui.click()
-    if idade[i] < 19:
-        pyautogui.moveTo(329,636, duration=0.25)
+    if idade[i] < 19 :
+        pyautogui.moveTo(696,794, duration=0.25)
         pyautogui.click()
-    elif idade[i] < 36:
-        pyautogui.moveTo(329,683, duration=0.25)
+    elif idade[i] <= 35:
+        pyautogui.moveTo(703,838, duration=0.25)
         pyautogui.click()
-    elif idade[i] < 51:
-        pyautogui.moveTo(329,683, duration=0.25)
+    elif idade[i] <= 50:
+        pyautogui.moveTo(701,884, duration=0.25)
         pyautogui.click()
-    elif idade[i] < 71:
-        pyautogui.moveTo(347,774, duration=0.25)
+    elif idade[i] <= 70:
+        pyautogui.moveTo(705,936, duration=0.25)
         pyautogui.click()
-    else:
-        pyautogui.moveTo(333,844, duration=0.25)
+    elif idade[i] >= 71:
+        pyautogui.moveTo(713,983, duration=0.25)
         pyautogui.click()
     
     if nota[i] == 1:
-        pyautogui.moveTo(408,770, duration=0.25)
+        pyautogui.moveTo(788,761, duration=0.25)
         pyautogui.click()
     elif nota[i] == 2:
-        pyautogui.moveTo(478,772, duration=0.25)
+        pyautogui.moveTo(867,766, duration=0.25)
         pyautogui.click()
     elif nota[i] == 3:
-        pyautogui.moveTo(557,774, duration=0.25)
+        pyautogui.moveTo(941,765, duration=0.25)
         pyautogui.click()
     elif nota[i] == 4:
-        pyautogui.moveTo(628,765, duration=0.25)
+        pyautogui.moveTo(1017,763, duration=0.25)
         pyautogui.click()
     elif nota[i] == 5:
-        pyautogui.moveTo(706,774, duration=0.25)
+        pyautogui.moveTo(1083,764, duration=0.25)
         pyautogui.click()
 
-    pyautogui.moveTo(353,959, duration=0.25)
+    pyautogui.moveTo(719,958, duration=0.25)
     pyautogui.click()
     pyautogui.write(comentario[i])
 
-    pyautogui.moveTo(309,1026, duration=0.25)
+    pyautogui.moveTo(691,1024, duration=0.25)
     pyautogui.click()
 
     time.sleep(1)
-    pyautogui.moveTo(327,245, duration=0.25)
+    pyautogui.moveTo(702,247, duration=0.25)
     pyautogui.click()
+
