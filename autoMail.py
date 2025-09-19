647602ad34229ef97eba3b1e4abe919d3e83c757
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-09-19 12:11:26 -0300

    atualização com email

--- autoMail.py
+++ autoMail.py
@@ -1,4 +1,4 @@
-import pyautogui,time,pyperclip
+import pyautogui,time,pyperclip, openpyxl
 
 pyautogui.PAUSE = 1
 pyautogui.alert('Abra o Chrome para começar o robo')
@@ -12,20 +12,28 @@ time.sleep(3)
 
 compose = pyautogui.locateOnScreen('compose.png', grayscale=True)
 
+wb = openpyxl.load_workbook('testeAuto.xlsx')
+sheet = wb['Planilha1']
+print(sheet)
+products = {}
+for row in sheet.iter_rows(min_row=2, max_row=10, min_col=1, max_col=3, values_only=True):
+    product ={
+        "to": row[0],
+        "Object": row[1],
+        "mail": row[2]
+    }
 
-for _ in range(3):
     pyautogui.moveTo(compose,duration=0.2)
     pyautogui.click()
 
-    pyautogui.write('joaormms@gmal.com',)
+    pyautogui.write(product['to'])
     pyautogui.press('enter')
 
     pyautogui.press('tab')
-    pyautogui.write('teste de automacao')
+    pyautogui.write(product['Object'])
 
     pyautogui.press('tab')
-    pyautogui.write('teste de automacao')   
+    pyautogui.write(product['mail'])   
 
     pyautogui.press('tab')
     pyautogui.press('enter')
-
